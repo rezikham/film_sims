a702121aefbe42d7b0abdedf958817a920c6930d
tqmane <nomu78@iwink.jp>
2026-02-10 02:01:36 +0900

    fix: Update versionCode and versionName to 8 and 1.0.5 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tqmane.filmsim"
         minSdk = 33
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.3"
+        versionCode = 8
+        versionName = "1.0.5"
     }
 
     buildTypes {
